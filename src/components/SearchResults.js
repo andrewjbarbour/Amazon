065231129a86c417eea720f9fbf8446cbb5030a9
Andrew Barbour <barbouraj@gmail.com>
2021-04-20 06:02:32 -0700

    optimized state management

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -237,7 +237,34 @@ export function SearchResults({location, productCount=0}){
                             </div>
                             <div class="search-results-inner-wrapper">
                                 <div class="search-results-inner-col-left" id="search-results-inner-col-left-row-1">
+                                <Link to={{
+                                    pathname: "/product",
+                                    state: {
+                                        productCount: productCount,
+                                        name: "Cracking the Coding Interview: 189 Programming Questions and Solutions 6th Edition",
+                                        author: "Gayle Laakmann McDowell",
+                                        bestseller: "Job Interviewing",
+                                        images: [cracking],
+                                        ratings: 4777,
+                                        releaseDate: "July 15, 2015",
+                                        cost1: 24.00,
+                                        cost2: 24.00,
+                                        usedCount: 37,
+                                        usedPrice: 20.82,
+                                        newCount: 8,
+                                        newPrice: 24.00,
+                                        listPrice: 39.95,
+                                        description: `I am not a recruiter. I am a software engineer. And as such, I know what it's like to be asked to whip up brilliant algorithms on the spot and then write flawless code on a whiteboard. I've been through this as a candidate and as an interviewer.`,
+                                        subheaderText: "Cracking the Coding Interview, 6th Edition is here to help you through this process, teaching you what you need to know and enabling you to perform at your very best. I've coached and interviewed hundreds of software engineers. The result is this book.",
+                                        subheaderText2: "",
+                                        subheaderText3: "Learn how to uncover the hints and hidden details in a question, discover how to break down a problem into manageable chunks, develop techniques to unstick yourself when stuck, learn (or re-learn) core computer science concepts, and practice on 189 interview questions and solutions.",
+                                        subheaderText4: "These interview questions are real; they are not pulled out of computer science textbooks. They reflect what's truly being asked at the top companies, so that you can be as prepared as possible.",
+                                        contents: ["189 programming interview questions, ranging from the basics to the trickiest algorithm problems.", "A walk-through of how to derive each solution, so that you can learn how to get there yourself.", "Hints on how to solve each of the 189 questions, just like what you would get in a real interview.",
+                                        "Five proven strategies to tackle algorithm questions, so that you can solve questions you haven't seen.", "Extensive coverage of essential topics, such as big O time, data structures, and core algorithms.",
+                                        "A behind the scenes look at how top companies like Google and Facebook hire developers.", "Techniques to prepare for and ace the soft side of the interview: behavioral questions.", "For interviewers and companies: details on what makes a good interview question and hiring process."]
+                                    }}}>
                                     <img class="search-result-img" id="search-result-img-2" src={searchResult2}></img>
+                                    </Link>
                                 </div>
                                 <div class="search-results-inner-col-right" id="search-results-inner-col-right-row-1">
                                     <span class="search-results-inner-col-header">Cracking the Coding Interview: 189 Programming Questions and Solutions</span>
